e084ae2cab067e3d7cb7d11ab9145a7ad0af3ea8
Juan Moraga <poluxfc@gmail.com>
2022-06-29 18:47:50 -0300

    update

--- cursos/javaScriptBasico/ejercicios/ejercicioTema10/controller.js
+++ cursos/javaScriptBasico/ejercicios/ejercicioTema10/controller.js
@@ -4,3 +4,8 @@ export const multiplica = (a, b) => a * b;
 
 export const imprimir = texto => console.log(texto);
 
+export const factorial = num => {
+    let res = 1;
+    for (num; num > 1; num--) { res *= num; }
+    return res
+}
